050592330a32bdd7c58e2b5f7ad144fb6889ac9f
GustavoLyra23 <gustavolyra23@gmail.com>
2024-11-11 22:07:46 -0300

    first commit

---
+++ src/main/java/org/gustavolyra/image_process_service/ImageProcessServiceApplication.java
@@ -0,0 +1,13 @@
+package org.gustavolyra.image_process_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageProcessServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageProcessServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gustavolyra/image_process_service/ImageProcessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gustavolyra.image_process_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageProcessServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
