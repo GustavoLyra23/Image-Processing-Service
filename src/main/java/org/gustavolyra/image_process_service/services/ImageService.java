8458e81a6263c2ea9b79da4cb0639f2c9e1c6f69
GustavoLyra23 <gustavolyra23@gmail.com>
2024-11-15 12:45:49 -0300

    implemented other transformations functions

--- src/main/java/org/gustavolyra/image_process_service/services/ImageService.java
+++ src/main/java/org/gustavolyra/image_process_service/services/ImageService.java
@@ -6,6 +6,8 @@ import org.gustavolyra.image_process_service.exceptions.ResourceNotFoundExceptio
 import org.gustavolyra.image_process_service.exceptions.ReverseProxyException;
 import org.gustavolyra.image_process_service.models.dto.ImageDataDto;
 import org.gustavolyra.image_process_service.models.dto.ImageDto;
+import org.gustavolyra.image_process_service.models.dto.transformations.Crop;
+import org.gustavolyra.image_process_service.models.dto.transformations.Filters;
 import org.gustavolyra.image_process_service.models.dto.transformations.Resize;
 import org.gustavolyra.image_process_service.models.dto.transformations.TransformationsDto;
 import org.gustavolyra.image_process_service.repositories.ImageRepository;
@@ -76,20 +78,31 @@ public class ImageService {
             return applyTransformations(s3File, transformations);
         } catch (IOException e) {
             log.error("Error fetching image", e);
-            throw new ReverseProxyException("Error fetching image");
+            throw new ReverseProxyException("Error fetching image from S3");
         }
     }
 
     public byte[] applyTransformations(byte[] image, TransformationsDto transformations) {
         try {
-            log.info("applying transformations");
-            ByteArrayInputStream inputStream = new ByteArrayInputStream(image);
-            BufferedImage buffImage = ImageIO.read(inputStream);
-            buffImage = applyResize(buffImage, transformations.getResize());
-            //converting BufferedImage to byte array...
+            log.info("Applying transformations...");
+            BufferedImage buffImage = ImageIO.read(new ByteArrayInputStream(image));
+            if (transformations.getResize() != null) {
+                buffImage = applyResize(buffImage, transformations.getResize());
+            }
+            if (transformations.getCrop() != null) {
+                buffImage = applyCrop(buffImage, transformations.getCrop());
+            }
+            if (transformations.getRotate() != 0) {
+                buffImage = applyRotate(buffImage, transformations.getRotate());
+            }
+            if (transformations.getFilters() != null) {
+                buffImage = applyFilters(buffImage, transformations.getFilters());
+            }
+
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             ImageIO.write(buffImage, "png", outputStream);
-            log.info("transformations applied");
+
+            log.info("Transformations applied successfully.");
             return outputStream.toByteArray();
         } catch (IOException e) {
             log.error("Error applying transformations", e);
@@ -98,11 +111,56 @@ public class ImageService {
     }
 
     public BufferedImage applyResize(BufferedImage image, Resize resize) {
-        log.info("applying resize");
-        if (resize != null) {
-            return Scalr.resize(image, Scalr.Method.QUALITY, resize.getWidth(), resize.getHeight());
-        } else {
-            return image;
+        log.info("Applying resize...");
+        return Scalr.resize(image, Scalr.Method.QUALITY, resize.getWidth(), resize.getHeight());
+    }
+
+    public BufferedImage applyCrop(BufferedImage image, Crop crop) {
+        log.info("Applying crop...");
+        return Scalr.crop(image, crop.getX(), crop.getY(), crop.getWidth(), crop.getHeight());
+    }
+
+    public BufferedImage applyRotate(BufferedImage image, int rotate) {
+        log.info("applying rotate");
+        return Scalr.rotate(image, Scalr.Rotation.CW_90);
+    }
+
+    public BufferedImage applyFilters(BufferedImage image, Filters filters) {
+        log.info("Applying filters...");
+        if (Boolean.TRUE.equals(filters.isGrayscale())) {
+            image = Scalr.apply(image, Scalr.OP_GRAYSCALE);
         }
+        if (Boolean.TRUE.equals(filters.isSepia())) {
+            image = applySepia(image);
+        }
+        return image;
+    }
+
+    public BufferedImage applySepia(BufferedImage image) {
+        log.info("Applying sepia...");
+        int width = image.getWidth();
+        int height = image.getHeight();
+        BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int p = image.getRGB(x, y);
+                newImage.setRGB(x, y, calculateSepiaColor(p));
+            }
+        }
+        return newImage;
+    }
+
+    private int calculateSepiaColor(int p) {
+        int a = (p >> 24) & 0xff;
+        int r = (p >> 16) & 0xff;
+        int g = (p >> 8) & 0xff;
+        int b = p & 0xff;
+
+        int tr = Math.min((int) (0.393 * r + 0.769 * g + 0.189 * b), 255);
+        int tg = Math.min((int) (0.349 * r + 0.686 * g + 0.168 * b), 255);
+        int tb = Math.min((int) (0.272 * r + 0.534 * g + 0.131 * b), 255);
+
+        return (a << 24) | (tr << 16) | (tg << 8) | tb;
     }
 }
